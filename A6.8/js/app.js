7103029023b19659149bb0345d10dd1a5d913804
pevgenyg <57189711+pevgenyg@users.noreply.github.com>
2019-11-30 08:33:33 +0300

    Update app.js

--- A6.8/js/app.js
+++ A6.8/js/app.js
@@ -1,12 +1,28 @@
+let i = 0;
+
 function handleButton() {
-  let i = 1;
+ i =  i+ 1;
+
+  $("#my-progress").width(i+"%");
+  }
+
+function handleButton1() {
+   i = i+3;
+
+  $("#my-progress").width(i+"%");
+  }
+
+  function handleButton2() {
+   i = i+17;
 
   $("#my-progress").width(i+"%");
   }
 
   function init() {
   $("#button").click(handleButton);
-}
+  $("#button1").click(handleButton1);
+  $("#button2").click(handleButton2);
 
+}
 
 $(document).ready(init);
